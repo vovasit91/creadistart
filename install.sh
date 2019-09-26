00b39844bc9514d11b4c718d485b6c2c1b58578d
Taivo <taivok@gmail.com>
2019-09-26 18:18:00 +0300

    fix install.sh script

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ docker-compose run --rm php composer update --prefer-dist
 
 docker-compose run --rm php composer install
 
-docker-compose up -d --force-recreate
-docker-compose run --rm php sh is-db-ready.sh "db" "php yii migrate"
-docker-compose run --rm php sh is-db-ready.sh "db_test" "php tests/bin/yii migrate"
+docker-compose up -d
+docker-compose run --rm php sh is-db-ready.sh "db" "php yii migrate --interactive=0"
+docker-compose run --rm php sh is-db-ready.sh "db_test" "php tests/bin/yii migrate --interactive=0"
